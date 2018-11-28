5303f164cb5a276df1b75ac63e980fdbc623ee62
sjh <986336902@qq.com>
2018-11-28 13:14:57 +0800

    注册/欢迎页/专家管理

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-document.write("<script language=javascript src='/static/js/admin.js'></script>");
+//document.write("<script language=javascript src='/static/js/admin.js'></script>");
 var baseUrl = 'http://192.168.10.99:8090', //',http://58.221.146.166:9200
      token = '',
      nickname='',
