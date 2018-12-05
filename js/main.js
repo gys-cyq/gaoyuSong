6bed3ff7681b7eb5625b9fb605f79111dd8cd40b
1258186598@qq.com <why920303>
2018-12-05 14:12:39 +0800

    修改界面

--- js/main.js
+++ js/main.js
@@ -49,14 +49,16 @@ function GetQueryString(name) {
     if (r != null) return unescape(r[2]); return null;
 }
 
+//手机号验证
 function checkMolie(str) {
-    var re = /^((13[0-9]{1})|(15[0-9]{1})|(18[0-9]{1}))+(\d{8})$/;
+    var re = /^((13[0-9]{1})|(15[0-9]{1})|(18[0-9]{1})|(14[0-9]{1})|(17[0-9]{1}))+(\d{8})$/;
     if (re.test(str)) {
         return true
     } else {
         return false;
     }
 }
+//联系电话验证
 function checkTel(phone) {
 
     if (!/^(\(\d{3,4}\)|\d{3,4}-|\s)?\d{7,14}$/.test(phone)) {
